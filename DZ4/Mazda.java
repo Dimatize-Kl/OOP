a4106602647f7ef9c037361be6c3e8c5972cba92
Dimatize-Kl <107035670+Dimatize-Kl@users.noreply.github.com>
2022-08-28 19:28:41 +0300

    Add files via upload

---
+++ DZ4/Mazda.java
@@ -0,0 +1,9 @@
+package com.kda.collection.DZ4;
+
+public class Mazda extends Car{
+
+    public Mazda(String brand, String model, int yearOfIssue, String color){
+        super(brand, model, yearOfIssue, color);
+    }
+
+}
