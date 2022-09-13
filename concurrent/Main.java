1c43912c50772504109de9cd3d82ad6a20885d14
Dimatize-Kl <107035670+Dimatize-Kl@users.noreply.github.com>
2022-09-13 16:42:40 +0300

    Add files via upload

---
+++ concurrent/Main.java
@@ -0,0 +1,33 @@
+package com.kda.concurrent;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        MageFire mageFire = new MageFire();
+        MageAir mageAir = new MageAir();
+        PlanetCrystal planetCrystal = new PlanetCrystal();
+        planetCrystal.setMageFire(mageFire);
+        planetCrystal.setMageAir(mageAir);
+
+        mageFire.setMageAir(mageAir);
+        mageFire.setPlanetCrystal(planetCrystal);
+
+        mageAir.setPlanetCrystal(planetCrystal);
+        mageAir.setMageFire(mageFire);
+
+        Thread threadOne = new Thread(planetCrystal);
+        Thread threadTwo = new Thread(mageFire);
+        Thread threadThree = new Thread(mageAir);
+
+        threadOne.start();
+        threadTwo.start();
+        threadThree.start();
+
+
+        threadOne.join();
+        threadTwo.join();
+        threadThree.join();
+
+
+    }
+}
