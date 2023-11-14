165b8099ff4ed007f8209439644314ed539c3495
Dimatize-Kl <107035670+Dimatize-Kl@users.noreply.github.com>
2023-11-14 21:49:58 +0300

    Delete DZ4 directory

--- DZ4/Mazda.java
+++
@@ -1,9 +0,0 @@
-package com.kda.collection.DZ4;
-
-public class Mazda extends Car{
-
-    public Mazda(String brand, String model, int yearOfIssue, String color){
-        super(brand, model, yearOfIssue, color);
-    }
-
-}
